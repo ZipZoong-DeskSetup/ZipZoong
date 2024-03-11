92b09ed1b69486e3b5117b563aa126e94b4b7edc
hon1770 <hon1770@naver.com>
2024-03-11 16:27:59 +0900

    Add: profileupload components #S10P22A204-115 #S10P22A204-143

--- frontend/zipjung/src/components/Common/PhotoUpload.tsx
+++ frontend/zipjung/src/components/Common/PhotoUpload.tsx
@@ -1,9 +1,58 @@
+"use client"
 import styles from '@/components/Common/PhotoUpload.module.scss'
+import Image from 'next/image'
+import { useState, useRef } from 'react'
+import { BiBorderRadius } from 'react-icons/bi';
 
 function PhotoUpload() {
+    // // selectedFile의 타입을 File | null로 변경
+    // const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+    // const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     // event.target.files가 null이 아닌 경우에만 파일을 설정
+    //     if (event.target.files && event.target.files.length > 0) {
+    //         setSelectedFile(event.target.files[0]);
+    //     }
+    // };
+
+    // console.log(selectedFile)
+
+    const [imgFile, setImgFile] = useState<string | null>(null);
+    const imgRef = useRef<HTMLInputElement>(null);
+
+
+    // 이미지 업로드 input의 onChange
+    const saveImgFile = () => {
+        if (imgRef.current && imgRef.current.files && imgRef.current.files[0]) {
+            const file = imgRef.current.files[0];
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = () => {
+                if (typeof reader.result === 'string') {
+                    setImgFile(reader.result);
+                }
+            };
+        }
+    };
+
     return (
         <div className={styles.photoUpload}>
-            사진
+            <input
+                type="file"
+                accept="image/*"
+                id="profileImg"
+                onChange={saveImgFile}
+                ref={imgRef}
+                hidden
+            />
+            <label className={styles.profileUploadLabel} htmlFor="profileImg">
+                <img
+                    src={imgFile ? imgFile : `/images/profileImg.png`}
+                    alt="프로필 이미지"
+                    className={styles.profileUploadImg}
+                />
+            </label>
+
         </div>
     )
 }
