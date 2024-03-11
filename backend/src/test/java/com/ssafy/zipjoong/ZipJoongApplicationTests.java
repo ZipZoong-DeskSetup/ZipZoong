6bd66cbb3f9b38c142b141cc538f49b3788853e8
서성원 <zsa332@gmail.com>
2024-03-11 10:59:00 +0900

    Feat : Backend Init

---
+++ backend/src/test/java/com/ssafy/zipjoong/ZipJoongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.zipjoong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipJoongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
