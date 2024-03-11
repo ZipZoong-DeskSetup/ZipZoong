854e975ef3575a436576d8204a8608de1da4e5a5
서성원 <zsa332@gmail.com>
2024-03-11 17:53:15 +0900

    git commit -m "S10P22A204-79 <제품, 조합등 좋아요 도메인 생성>"

---
+++ backend/src/main/java/com/ssafy/zipjoong/like/model/CombinationLikeId.java
@@ -0,0 +1,19 @@
+package com.ssafy.zipjoong.like.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Getter
+@Builder
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+public class CombinationLikeId implements Serializable {
+    private Integer userId;
+    private Long combinationId;
+}
