66a17cb7538f78a5ec9dccb6d8bb25935913310f
hyunjeong1 <hjeong.r@gmail.com>
2024-03-13 14:42:54 +0900

    Add: NavBar component and Modify modal image #S10P22A204-137

--- frontend/zipjung/src/app/recommend/page.tsx
+++ frontend/zipjung/src/app/recommend/page.tsx
@@ -1,7 +1,4 @@
-import Form from "@/components/MyPage/UpdateInfo"
-export default function Page() {
+export default function Page() {;
   
-    return <><h2>나는 추천 목록</h2>
-    <Form/>
-    </>
+    return <><h2>나는 추천 목록</h2></>
     };
