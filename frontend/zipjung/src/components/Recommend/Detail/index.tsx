a209a42954e19a8ef9f78eef0913c0ddc9534c76
hon1770 <hon1770@naver.com>
2024-03-14 17:31:39 +0900

    WIP:recommendDetailPage

--- frontend/zipjung/src/components/Recommend/Detail/index.tsx
+++ frontend/zipjung/src/components/Recommend/Detail/index.tsx
@@ -1,12 +1,24 @@
+'use client'
 import GoBackButton from '@/components/Common/GoBackButton';
+import useRecommendStore from '@/stores/recommend';
 import styles from '@/components/Recommend/Detail/index.module.scss';
 
 function Form() {
+    const { ZustandRecommendDetail } = useRecommendStore();
+
+    console.log(ZustandRecommendDetail)
+
     return (
-        <div>
+        <div className={styles.detailContain}>
             추천상세
-            <div>
-                <GoBackButton />
+            <div className={styles.detailDiv}>
+                <div>
+                    <GoBackButton />
+                </div>
+                <div>
+                    추천 {ZustandRecommendDetail?.id}
+                </div>
+
             </div>
         </div>
     );
