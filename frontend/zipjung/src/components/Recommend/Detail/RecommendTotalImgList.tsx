eef27d73a40194281a58d213aeec759493aacbcd
hon1770 <hon1770@naver.com>
2024-03-15 17:49:36 +0900

    WIP:recommendDetailPage

---
+++ frontend/zipjung/src/components/Recommend/Detail/RecommendTotalImgList.tsx
@@ -0,0 +1,12 @@
+import styles from '@/components/Recommend/Detail/RecommendTotalImgList.module.scss';
+
+
+function RecommendTotalImgList() {
+    return(
+        <>
+            이미지
+        </>
+    );
+};
+
+export default RecommendTotalImgList;
--- frontend/zipjung/src/components/Recommend/Detail/index.tsx
+++ frontend/zipjung/src/components/Recommend/Detail/index.tsx
@@ -1,7 +1,11 @@
 'use client'
 import GoBackButton from '@/components/Common/GoBackButton';
 import useRecommendStore from '@/stores/recommend';
+import ShareButton from '@/components/Common/Recommend/ShareButton';
+import RecommendLikeButton from '@/components/Common/Recommend/RecommendLikeButton';
+import RecommendTotalImgList from '@/components/Recommend/Detail/RecommendTotalImgList';
 import styles from '@/components/Recommend/Detail/index.module.scss';
+
 
 function Form() {
     const { ZustandRecommendDetail } = useRecommendStore();
@@ -9,14 +13,24 @@ function Form() {
     console.log(ZustandRecommendDetail)
 
     return (
-        <div className={styles.detailContain}>
-            추천상세
-            <div className={styles.detailDiv}>
+        <div className={styles.detailContains}>
+            <div className={styles.detailContain}>
                 <div>
                     <GoBackButton />
                 </div>
-                <div>
-                    추천 {ZustandRecommendDetail?.id}
+                <div className={styles.detailDiv}>
+                    <div className={styles.detailHead}>
+                        <div className={styles.detailId}>
+                            추천 {ZustandRecommendDetail?.id}
+                        </div>
+                        <div className={styles.buttons}>
+                            <ShareButton />
+                            <RecommendLikeButton key={ZustandRecommendDetail?.id} itemId={ZustandRecommendDetail?.id} />
+                        </div>
+                    </div>
+                    <div>
+                        <RecommendTotalImgList />
+                    </div>
                 </div>
 
             </div>
