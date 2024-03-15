71619f6d9d69884ca9f57c63c90c2c79049e7574
이수현 <suhyeon9120@gmail.com>
2024-03-15 12:30:44 +0900

    ADD: CustomOAuth2User 추가 #S10P22A204-89

---
+++ backend/src/main/java/com/ssafy/zipjoong/security/oauth2/user/CustomOAuth2User.java
@@ -0,0 +1,41 @@
+package com.ssafy.zipjoong.security.oauth2.user;
+
+import com.ssafy.zipjoong.user.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+
+    private User user;
+    private Collection<? extends GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
+
+    public CustomOAuth2User(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    public CustomOAuth2User(User user, Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes) {
+        this.user = user;
+        this.authorities = authorities;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return user.getUserNickname();
+    }
+}
