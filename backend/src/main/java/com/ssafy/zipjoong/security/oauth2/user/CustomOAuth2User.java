0da48f2449cc69367129d3d182f3788293d64ded
이수현 <suhyeon9120@gmail.com>
2024-03-16 17:41:43 +0900

    FIX: getAuthorities 메서드의 반환값을 null에서 authorities로 수정 #S10P22A204-89

--- backend/src/main/java/com/ssafy/zipjoong/security/oauth2/user/CustomOAuth2User.java
+++ backend/src/main/java/com/ssafy/zipjoong/security/oauth2/user/CustomOAuth2User.java
@@ -39,7 +39,7 @@ public class CustomOAuth2User implements OAuth2User {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
