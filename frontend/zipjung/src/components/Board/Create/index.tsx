70ec1633623c145bd2800d2e31caf1f2114dab2c
hon1770 <hon1770@naver.com>
2024-03-21 13:22:32 +0900

    WIP:boardPage:

--- frontend/zipjung/src/components/Board/Create/index.tsx
+++ frontend/zipjung/src/components/Board/Create/index.tsx
@@ -7,12 +7,12 @@ import CreateButton from '@/components/Board/Create/CreateButton';
 import useUserInfoStore from '@/stores/userInfo';
 import styles from '@/components/Board/Create/index.module.scss';
 
-interface FormData {
-  boardTitle: string;
-  boardContent: string;
-  boardCreator: string;
-  boardCreatorId: number;
-}
+// interface FormData {
+//   boardTitle: string;
+//   boardContent: string;
+//   boardCreator: string;
+//   boardCreatorId: number;
+// }
 
 function Form() {
   const [title, setTitle] = useState<string>('');
@@ -30,6 +30,12 @@ function Form() {
         <div style={{display: 'flex'}}>
           <div className="ck ck-editor__main" style={{width: '100%'}}></div>
         </div>
+      </div>
+      <div>
+        {title}
+        {data}
+        {ZustandNickname}
+        {ZustandId}
       </div>
       <div>
         <CreateButton />
