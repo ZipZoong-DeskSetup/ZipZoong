464587778041540113630d536541b4cf1f4b029b
서성원 <zsa332@gmail.com>
2024-03-21 17:12:03 +0900

    feat: Add constructor to ResponseDto #S10P22A204-157

--- backend/src/main/java/com/ssafy/zipjoong/util/dto/ResponseDto.java
+++ backend/src/main/java/com/ssafy/zipjoong/util/dto/ResponseDto.java
@@ -7,7 +7,13 @@ public class ResponseDto {
     private String message;
     private Object data;
 
+    public ResponseDto(){}
     public ResponseDto(String message) {
         this.message = message;
+        this.data = null;
+    }
+    public ResponseDto(String message, Object data){
+        this.message = message;
+        this.data = data;
     }
 }
