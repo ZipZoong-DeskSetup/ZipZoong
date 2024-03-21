912cccb8a2aef7f8ef76b64010027bc5f6aa98bc
hyunjeong1 <hjeong.r@gmail.com>
2024-03-21 18:02:15 +0900

    Feat: Monitor,Mouse,KeyBoard button component #S10P22A204-136

---
+++ frontend/zipjung/src/components/MyPage/Combination/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const CombinationContainer = styled.div`
+  background-color: red;
+  width: 100%;
+  height: 300px;
+`;
+const CombinationForm = () => {
+  return <CombinationContainer>조합</CombinationContainer>;
+};
+export default CombinationForm;
