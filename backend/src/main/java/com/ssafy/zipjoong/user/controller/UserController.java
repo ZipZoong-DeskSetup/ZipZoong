99536aa8b7d8bb307747d01267798e5496814769
이수현 <suhyeon9120@gmail.com>
2024-03-27 16:39:15 +0900

    FEAT: UserController 추가 #S10P22A204-95

---
+++ backend/src/main/java/com/ssafy/zipjoong/user/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.ssafy.zipjoong.user.controller;
+
+import com.ssafy.zipjoong.security.jwt.utils.JwtUtils;
+import com.ssafy.zipjoong.user.dto.UserImgUpdateRequest;
+import com.ssafy.zipjoong.user.dto.UserNicknameUpdateRequest;
+import com.ssafy.zipjoong.user.service.UserService;
+import com.ssafy.zipjoong.util.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    // 내 정보 조회
+    @GetMapping("/info")
+    public ResponseEntity<ResponseDto> getUserInfo(@RequestHeader("Authorization") String authorizationToken) {
+        String userId = JwtUtils.getUserId(authorizationToken);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("성공적으로 유저 정보를 조회하였습니다.", userService.getUserInfo(userId)));
+    }
+
+    // 닉네임 중복 여부 확인
+    @GetMapping("/nickname/check/{nickname}")
+    public ResponseEntity<ResponseDto> checkNicknameDuplication(@PathVariable(name = "nickname") String nickname) {
+        boolean isAvailable = !userService.checkNicknameDuplication(nickname);
+        Map<String, Boolean> response = Collections.singletonMap("isAvailable", isAvailable);
+
+        if(isAvailable) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("사용가능한 닉네임 입니다.", response));
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseDto("중복된 닉네임 입니다.", response));
+        }
+    }
+
+    // 닉네임 변경
+    @PostMapping("/nickname")
+    public ResponseEntity<ResponseDto> updateNickname(@RequestHeader("Authorization") String authorizationToken,
+                                                      @RequestBody UserNicknameUpdateRequest updateRequest) {
+        String userId = JwtUtils.getUserId(authorizationToken);
+        userService.updateNickname(userId, updateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("성공적으로 닉네임을 변경하였습니다."));
+    }
+
+    // 프로필 사진 변경
+    @PostMapping("/profile")
+    public ResponseEntity<ResponseDto> updateProfile(@RequestHeader("Authorization") String authorizationToken,
+                                                     @RequestBody UserImgUpdateRequest updateRequest) {
+        System.out.println(updateRequest);
+        String userId = JwtUtils.getUserId(authorizationToken);
+        userService.updateUserImg(userId, updateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("성공적으로 프로필 사진을 변경하였습니다."));
+    }
+
+}
