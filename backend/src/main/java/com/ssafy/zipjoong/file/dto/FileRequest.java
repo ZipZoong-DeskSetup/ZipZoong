fe516defa4e985cdca8ab5d0870727823a8d5402
이수현 <suhyeon9120@gmail.com>
2024-03-27 17:19:31 +0900

    ADD: @Builder 추가 #S10P22A204-95

--- backend/src/main/java/com/ssafy/zipjoong/file/dto/FileRequest.java
+++ backend/src/main/java/com/ssafy/zipjoong/file/dto/FileRequest.java
@@ -2,9 +2,11 @@ package com.ssafy.zipjoong.file.dto;
 
 import com.ssafy.zipjoong.board.domain.Board;
 import com.ssafy.zipjoong.file.domain.File;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class FileRequest {
     private String filePath;
     private Integer boardId;
