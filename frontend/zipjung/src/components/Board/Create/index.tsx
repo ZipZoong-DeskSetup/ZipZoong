84d745c86c518358abddbcca41e92a8378ab64bc
hon1770 <hon1770@naver.com>
2024-03-28 18:03:25 +0900

    WIP:ChooseRecoomend

--- frontend/zipjung/src/components/Board/Create/index.tsx
+++ frontend/zipjung/src/components/Board/Create/index.tsx
@@ -5,6 +5,7 @@
 import {useState} from 'react';
 import axios from 'axios';
 import TitleInput from '@/components/Board/Create/TitleInput';
+import ChooseRecommendButton from '@/components/Board/ChooseRecommendButton';
 import QuillEditor from '@/components/Board/Create/QuillEditor';
 import GoBackButton from '@/components/Common/GoBackButton';
 import CreateButton from '@/components/Board/Create/CreateButton';
@@ -14,15 +15,28 @@ import styles from '@/components/Board/Create/index.module.scss';
 function Form() {
   const [formTitle, setFormTitle] = useState<string>('');
   const [formContent, setFormContent] = useState<string>('');
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   const {ZustandNickname, ZustandId} = useUserInfoStore();
 
+  // 제목 변경
   const handleChangeTitle = (title: string) => {
     setFormTitle(title);
   };
 
+  // 내용 변경
   const handleChangeContent = (content: string) => {
     setFormContent(content);
+  };
+
+  // 모달
+  const handleOpenModal = () => {
+    axios.get()
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
   const goCreate = async () => {
@@ -56,6 +70,13 @@ function Form() {
     <div className={styles.createDiv}>
       <div className={styles.createTitle}>
         <TitleInput onChange={handleChangeTitle} />
+      </div>
+      <div>
+        {!showModal ? (
+          <ChooseRecommendButton onClick={handleOpenModal} />
+        ) : (
+          '리스트'
+        )}
       </div>
       <div className={styles.createContent}>
         <QuillEditor onChange={handleChangeContent} />
