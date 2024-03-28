3b36ba120c745d881868a103185c8035a4775497
이수현 <suhyeon9120@gmail.com>
2024-03-28 09:22:29 +0900

    ADD: 댓글 Swagger 관련 코드 추가 #S10P22A204-265

--- backend/src/main/java/com/ssafy/zipjoong/comment/controller/CommentController.java
+++ backend/src/main/java/com/ssafy/zipjoong/comment/controller/CommentController.java
@@ -5,6 +5,8 @@ import com.ssafy.zipjoong.comment.dto.CommentUpdateRequest;
 import com.ssafy.zipjoong.comment.service.CommentService;
 import com.ssafy.zipjoong.security.jwt.utils.JwtUtils;
 import com.ssafy.zipjoong.util.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,17 +17,20 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/comment")
+@Tag(name = "댓글 API", description = "댓글 API")
 public class CommentController {
     private final CommentService commentService;
 
     // 게시글별 댓글 전체 조회
     @GetMapping("/byBoard/{boardId}")
+    @Operation(summary = "게시글별 댓글 전체 조회", description = "게시글 ID를 이용하여 해당 게시글의 댓글 전체 조회")
     public ResponseEntity<ResponseDto> getCommentsByBoard(@PathVariable(name = "boardId") int boardId) {
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("성공적으로 해당 게시글의 댓글 목록을 조회하였습니다.", commentService.getAllByBoard(boardId)));
     }
 
     // 댓글 작성
     @PostMapping
+    @Operation(summary = "댓글 작성", description = "댓글 작성")
     public ResponseEntity<ResponseDto> createComment(@RequestHeader("Authorization") String authorizationToken,
                                                    @RequestBody CommentCreateRequest commentCreateRequest) {
         String userId = JwtUtils.getUserId(authorizationToken);
@@ -35,6 +40,7 @@ public class CommentController {
 
     // 댓글 수정
     @PutMapping("/{commentId}")
+    @Operation(summary = "댓글 수정", description = "댓글 ID를 이용하여 댓글 수정")
     public ResponseEntity<ResponseDto> updateComment(@PathVariable(name = "commentId") long commentId,
                                                    @RequestHeader("Authorization") String authorizationToken,
                                                    @RequestBody CommentUpdateRequest commentUpdateRequest) {
@@ -45,6 +51,7 @@ public class CommentController {
 
     // 댓글 삭제
     @PostMapping("/{commentId}")
+    @Operation(summary = "댓글 삭제", description = "댓글 ID를 이용하여 댓글 삭제")
     public ResponseEntity<ResponseDto> deleteComment(@PathVariable(name = "commentId") long commentId,
                                                    @RequestHeader("Authorization") String authorizationToken) {
         String userId = JwtUtils.getUserId(authorizationToken);
