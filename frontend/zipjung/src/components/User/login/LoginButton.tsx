2c1725001c58694d51da498c905712aec2820c44
hyunjeong1 <hjeong.r@gmail.com>
2024-04-02 10:29:23 +0900

    Feat: Add token set function and routing user

--- frontend/zipjung/src/components/User/login/LoginButton.tsx
+++ frontend/zipjung/src/components/User/login/LoginButton.tsx
@@ -11,16 +11,13 @@ function LoginButton({
   loginLink: string;
   loginImg: string;
 }) {
-  // TODO: 요청 응답 후 Zustand 닉네임, 이미지, 로그인 상태변화 하기
-  const loginHandler = () => {
-    window.location.href = loginLink;
-  };
-
   return (
     <div>
-      <button onClick={loginHandler}>
-        <img src={loginImg} alt="로그인" className={styles.loginButton} />
-      </button>
+      <a href={loginLink}>
+        <button>
+          <img src={loginImg} alt="로그인" className={styles.loginButton} />
+        </button>
+      </a>
     </div>
   );
 }
