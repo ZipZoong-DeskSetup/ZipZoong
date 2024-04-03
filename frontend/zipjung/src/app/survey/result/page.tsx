fab0ad4fb1a729c7659d67b750023dbacf37c240
유수안 <awetumnn@gmail.com>
2024-04-03 14:16:46 +0900

    FEAT : Footer 추가 및 적용 #S10P22A204-310

--- frontend/zipjung/src/app/survey/result/page.tsx
+++ frontend/zipjung/src/app/survey/result/page.tsx
@@ -1,9 +1,11 @@
+import Footer from '@/components/Common/Footer/Footer';
 import Form from '@/components/Survey/Result';
 
 export default function Page() {
   return (
     <>
       <Form />
+      <Footer></Footer>
     </>
   );
 }
