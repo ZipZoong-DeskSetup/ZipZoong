e8b65893489d3069f45e3c9674e9a6cc11157382
hyunjeong1 <hjeong.r@gmail.com>
2024-04-04 01:21:56 +0900

    Feat:connect api

--- frontend/zipjung/src/components/MyPage/UpdateInfo/ChangeImg.tsx
+++ frontend/zipjung/src/components/MyPage/UpdateInfo/ChangeImg.tsx
@@ -1,19 +1,20 @@
 /* eslint-disable no-console */
-// import {useState} from 'react';
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import Image from 'next/image';
-// import axios from 'axios';
+import axios from 'axios';
 import styles from '@/components/MyPage/UpdateInfo/ChangeImg.module.scss';
 import useUserInfoStore from '@/stores/userInfo';
 
 const ChangeImg = ({pUserImg}: {pUserImg: string}) => {
-  const {ZustandImageUrl, setZustandImageUrl} = useUserInfoStore();
+  const {ZustandImageUrl, setZustandImageUrl, ZustandToken} =
+    useUserInfoStore();
   const imgRef = useRef<HTMLInputElement>(null);
-  // const [presentImgUrl, setPresentImgUrl] = useState<string>(pUserImg);
-  console.log(pUserImg);
+  const [presentImgUrl, setPresentImgUrl] = useState<string>(pUserImg);
+  const [token, setToken] = useState<string>(ZustandToken);
   useEffect(() => {
-    // setPresentImgUrl(ZustandImageUrl);
-  }, [ZustandImageUrl]);
+    setPresentImgUrl(ZustandImageUrl);
+    setToken(ZustandToken);
+  }, [ZustandImageUrl, ZustandToken, token]);
 
   const saveImgFile = () => {
     // console.log('File');
@@ -24,19 +25,22 @@ const ChangeImg = ({pUserImg}: {pUserImg: string}) => {
       setZustandImageUrl(imageUrl);
       reader.readAsDataURL(file);
       reader.onloadend = () => {
-        if (typeof reader.result === 'string') {
-          // TODO: 서버에도 저장
-          // axios
-          //   .post(`${process.env.NEXT_PUBLIC_BASE_URL}/profile`, {
-          //     userImg: reader.result,
-          //   })
-          //   .then(response => {
-          //     console.log(response);
-          //   })
-          //   .catch(Error => {
-          //     console.error(Error);
-          //   });
-        }
+        const formData = new FormData();
+        formData.append('userImg', file);
+
+        axios
+          .post(`${process.env.NEXT_PUBLIC_BASE_URL}/user/profile`, formData, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'multipart/form-data',
+            },
+          })
+          .then(response => {
+            console.log(response);
+          })
+          .catch(Error => {
+            console.error(Error);
+          });
       };
     }
   };
@@ -49,20 +53,26 @@ const ChangeImg = ({pUserImg}: {pUserImg: string}) => {
         <div className={styles.profileImageContainer}>
           <Image
             className={styles.profileImg}
-            src={ZustandImageUrl}
+            src={presentImgUrl}
             alt="프로필 사진"
             width={200}
             height={200}
           />
         </div>
-        <input
-          type="file"
-          accept="image/*"
-          id="profileImg"
-          onChange={saveImgFile}
-          ref={imgRef}
-          hidden
-        />
+        <form
+          method="POST"
+          encType="multipart/form-data"
+          action={`${process.env.NEXT_PUBLIC_BASE_URL}/user/profile`}
+        >
+          <input
+            type="file"
+            accept="image/*"
+            id="profileImg"
+            onChange={saveImgFile}
+            ref={imgRef}
+            hidden
+          />
+        </form>
         <button className={styles.changButton}>
           <label className={styles.changeLabel} htmlFor="profileImg">
             <div className={styles.sort}>변경</div>
