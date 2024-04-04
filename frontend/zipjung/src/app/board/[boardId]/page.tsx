28b8050f8efe41b34bcb8989a4b1ad7a4124023a
hyunjeong1 <hjeong.r@gmail.com>
2024-04-04 15:31:08 +0900

    Fix:Header

--- frontend/zipjung/src/app/board/[boardId]/page.tsx
+++ frontend/zipjung/src/app/board/[boardId]/page.tsx
@@ -1,10 +1,8 @@
 import Form from '@/components/Board/[BoardId]';
-import Header from '@/components/Common/Header';
 
 export default function Page() {
   return (
     <>
-      <Header />
       <Form />
     </>
   );
