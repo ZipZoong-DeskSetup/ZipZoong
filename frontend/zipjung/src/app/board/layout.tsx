a96e15a4d32127de8767026c80c765746c6f1370
Yoosuean <awetumnn@gmail.com>
2024-04-04 14:15:17 +0900

    FIX : Footer 삭제 및 Header 위치 조정

--- frontend/zipjung/src/app/board/layout.tsx
+++ frontend/zipjung/src/app/board/layout.tsx
@@ -3,9 +3,6 @@ import Header from '@/components/Common/Header';
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <>
-      <header>
-        <Header />
-      </header>
       <body>{children}</body>
     </>
   );
--- frontend/zipjung/src/app/board/page.tsx
+++ frontend/zipjung/src/app/board/page.tsx
@@ -1,11 +1,12 @@
 import Form from '@/components/Board';
+import Header from '@/components/Common/Header';
 // import Footer from '@/components/Common/Footer/Footer';
 
 export default function Page() {
   return (
     <>
+    <Header />
       <Form />
-      {/* <Footer></Footer> */}
     </>
   );
 }
