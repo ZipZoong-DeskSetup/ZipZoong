d0e67c3ef1e4bf759a93992cf4ccb9b5dd1a8b84
hyunjeong1 <hjeong.r@gmail.com>
2024-04-04 11:22:33 +0900

    Feat: mypage

--- frontend/zipjung/src/app/board/page.tsx
+++ frontend/zipjung/src/app/board/page.tsx
@@ -1,11 +1,9 @@
 import Form from '@/components/Board';
-import Footer from '@/components/Common/Footer/Footer';
 
 export default function Page() {
   return (
     <>
       <Form />
-      <Footer></Footer>
     </>
   );
 }
